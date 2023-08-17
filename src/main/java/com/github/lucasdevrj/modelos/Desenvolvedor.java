479a241ada0ff8a6502c5ac0d2ad64240a27ad81
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-17 17:13:52 -0300

    adição de atributos na classe Desenvolvedor

--- src/main/java/com/github/lucasdevrj/modelos/Desenvolvedor.java
+++ src/main/java/com/github/lucasdevrj/modelos/Desenvolvedor.java
@@ -1,5 +1,10 @@
 package com.github.lucasdevrj.modelos;
 
 public class Desenvolvedor {
-
+	
+	private Integer id; 
+	private String nome;
+	private String area;
+	private String tecnologias;
+	private boolean empregado;
 }
