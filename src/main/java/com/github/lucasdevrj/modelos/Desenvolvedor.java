aa85553c2753233d006ead6fef0fdeb31d7ae49e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-17 17:17:04 -0300

    adição de anotações persistence

--- src/main/java/com/github/lucasdevrj/modelos/Desenvolvedor.java
+++ src/main/java/com/github/lucasdevrj/modelos/Desenvolvedor.java
@@ -1,10 +1,21 @@
 package com.github.lucasdevrj.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "desenvolvedores")
 public class Desenvolvedor {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id; 
 	private String nome;
 	private String area;
 	private String tecnologias;
-	private boolean empregado;
+	private String graduacao;
+	private String cargo;
 }
